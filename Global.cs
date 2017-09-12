a5739128acad271f42dcd0ad1fd56d188163238c
Dipak Akhade <dipak.a.akhade1291@gmail.com>
2017-09-12 23:55:38 +0530

    IMS-MAIN branch creation.

--- Global.cs
+++ Global.cs
@@ -14,11 +14,6 @@ namespace InventoryManagement
         //public static 
         //public static 
         //public static 
-        //public static 
-        //public static 
-        //public static 
-        //public static 
-        //public static 
-        //public static 
+        
     }
 }
